795f953ef590e79125366db57dedac85fd498d26
Blake Grotewold <blake.grotewold@gmail.com>
2023-02-10 14:33:00 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir $HOME/projects
-
-dname="Foxio"
-demail="hello@fox.io"
-
-read name "Git Name ($dname): "
-read email "Git Email ($demail): "
-
-git config --global user.name ${name-$dname}
-git config --global user.email ${email-$demail}
-
-ssh-keygen -t rsa -b 4096 -C ${email-$demail}
-eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_rsa
-pbcopy < $HOME/.ssh/id_rsa.pub
-echo "SSH key copied."
-
-# copy vs-code settings
-mkdir -p "$HOME/Library/Application Support/Code/User"
-mv "$HOME/vsc_settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-
 xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -34,24 +13,19 @@ brew install yarn
 brew install --cask alfred
 brew install --cask forklift
 brew install --cask docker
-brew install --cask dropbox
 brew install --cask firefox
-brew install --cask font-fira-code
 brew install --cask google-chrome
 brew install --cask local
-brew install --cask mysqlworkbench
 brew install --cask ngrok
-brew install --cask psequel
+brew install --cask postico
 brew install --cask slack
 brew install --cask sourcetree
 brew install --cask spectacle
 brew install --cask spotify
-brew install --cask virtualbox
 brew install --cask visual-studio-code
 brew install --cask zoom
 
 modules="
-peterjausovec.vscode-docker
 adamvoss.yaml
 bungcip.better-toml
 dbaeumer.vscode-eslint
